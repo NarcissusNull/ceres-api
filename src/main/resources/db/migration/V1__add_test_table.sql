4087d5714a67136e3d1b3f6a78f3104e6ba5aaa3
NarcissusNull <NarcissusNull@outlook.com>
2021-04-29 19:30:07 +0800

    update goods table

--- src/main/resources/db/migration/V1__add_test_table.sql
+++ src/main/resources/db/migration/V1__add_test_table.sql
@@ -1,7 +1,9 @@
-CREATE TABLE GOODS(
+CREATE TABLE goods(
   id INT primary key NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
-  `type` INT NOT NULL,
-  `createdAt` DATETIME DEFAULT NOW(),
-  `updatedAt` DATETIME DEFAULT NOW()
+  `price` DECIMAL(10,2) not null,
+  `type` INT NULL,
+  `describe` VARCHAR(2000) null,
+  `created_at` DATETIME DEFAULT NOW(),
+  `updated_at` DATETIME DEFAULT NOW()
 );
